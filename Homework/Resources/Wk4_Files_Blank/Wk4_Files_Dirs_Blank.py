16676c919293776b87c9e40ad0e572a8dba1b0ab
4AM365 <wtcraig@live.com>
2023-02-02 16:22:23 -0700

    New HW!

---
+++ Homework/Resources/Wk4_Files_Blank/Wk4_Files_Dirs_Blank.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+@author: bstarly
+"""
+
+    
+'''
+Qn1
+#Write a Program to Write The Given Strings to a File
+#n1='\n'
+#lines = 'Binil', n1, 'Starly', 'is', n1, 'teaching', n1, 'Python', n1, 'today'
+
+'''
+
+
+
+
+
+'''
+Qn2
+#Write a function CodeWord(filename) that reads a file ‘CNCFile.txt’ containing a large amount of text.
+#For every instance of the word – CNC contained in the file, replace this word with the word – ‘XYZ’. Output this new text to another file – “CodedText.txt”.
+#Also output the number of times the word – CNC appeared in the file to the console screen.
+'''
+
+
+
+
+
+
+'''
+QN3:
+A: Read the contents (names of the files) of a folder that contains files and sub-folders
+B: Calculate the number of JPEG and JSON type files contained. No other file types to be read.
+C: Print the file sizes of only the JPEG files contained in the folder
+'''
+
+
+
+
+
+'''
+Qn4:
+A: Read the contents of all the JSON files within the sample folder.
+B: Read all of the volume values inside each of the json files.
+C: Report on the total volume by summing all of these values.
+'''
+
+
+
+
+
+'''
+#Qn5
+A: Extract the content of the zip file.
+B: Count the number of units sold
+C: Find the country that obtained the maximum revenue
+D: Output a JSON file that lists each Item_Type and the countries that sold those items
+   as a dictionary, in the following format 
+   ItemType: List of countries that sold that item 
+'''
