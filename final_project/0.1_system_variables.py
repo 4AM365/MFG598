acf13d18161a948c227e29c187364af35ff55c4b
4AM365 <wtcraig@live.com>
2023-04-10 15:27:53 -0700

    Added structure, began defining modules.

---
+++ final_project/0.1_system_variables.py
@@ -0,0 +1,9 @@
+#NTFS database of source files
+def variables(database, bins, bosch_filepath):
+    database = 'H:\LOSS1\EDR\Downloaded Files'
+    bins = 10
+    bosch_filepath = 'C:\Program Files (x86)\Bosch\Crash Data Retrieval\CDR.EXE'
+
+    return database, bins, bosch_filepath
+
+
---
+++ final_project/2.1_exploratory_data_analysis.py
@@ -0,0 +1,3 @@
+from speedml import SpeedML as sml
+from speedml.feature import Feature
+from speedml.model import Model
