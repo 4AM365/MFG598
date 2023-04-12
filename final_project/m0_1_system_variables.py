ab651be367075e70158d887b78a3213118d12999
4AM365 <wtcraig@live.com>
2023-04-11 19:44:39 -0700

    ??

--- final_project/m0_1_system_variables.py
+++ final_project/m0_1_system_variables.py
@@ -1,9 +1,10 @@
 #NTFS database of source files
-def variables(database, bins, bosch_filepath):
+def variables():
     database = 'H:\LOSS1\EDR\Downloaded Files'
     bins = 10
     bosch_filepath = 'C:\Program Files (x86)\Bosch\Crash Data Retrieval\CDR.EXE'
 
     return database, bins, bosch_filepath
+    
 
 
