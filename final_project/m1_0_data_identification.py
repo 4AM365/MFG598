3927b94756a413f0a4636877e436868a041935ee
WCraig <38740322+crgsquared@users.noreply.github.com>
2023-04-13 15:30:38 -0700

    ??

--- final_project/m1_0_data_identification.py
+++ final_project/m1_0_data_identification.py
@@ -1,5 +1,8 @@
 import os
-from m0_1_system_variables import raw_data_path, scanned_file_list, file_staging_list
+# from m0_1_system_variables import raw_data_path, scanned_file_list, file_staging_list
+from m0_1_system_variables import scanned_file_list, file_staging_list
+
+raw_data_path = r'H:\LOSS1\EDR\Downloaded Files'
 
 for root, dirs, files in os.walk(raw_data_path):
     for filename in files:
@@ -12,4 +15,4 @@ for root, dirs, files in os.walk(raw_data_path):
                 file_staging_list.append(filename)
                 print(f'New file detected: {os.path.join(root, filename)}')
 
-print(file_staging_list)
+print(file_staging_list) 
