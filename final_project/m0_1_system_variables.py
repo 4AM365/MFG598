322141a1f65cd60d39b98f557fb7b928e289cbce
4AM365 <wtcraig@live.com>
2023-04-12 21:37:37 -0700

    ??

--- final_project/m0_1_system_variables.py
+++ final_project/m0_1_system_variables.py
@@ -1,9 +1,12 @@
 #NTFS database of source files
+
+import os
+
 raw_data_path = 'H:\LOSS1\EDR\Downloaded Files'
 
 bins = 10
 
-bosch_filepath = 'C:\Program Files (x86)\Bosch\Crash Data Retrieval\CDR.EXE'
+bosch_filepath = os.path.join('C:', 'Program Files (x86)', 'Bosch', 'Crash Data Retrieval', 'CDR.EXE')
 
 #This is the list of files that have already been imported into the system.
 scanned_file_list = []
--- final_project/m1_1_data_type_standardization.py
+++ final_project/m1_1_data_type_standardization.py
@@ -1,1 +1,22 @@
 import pyautogui
+
+import psutil
+
+import subprocess
+
+from m0_1_system_variables import bosch_filepath
+
+def check_process(process_name):
+    """Check if a process is running on Windows"""
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] == process_name:
+            return True
+    return False
+
+# Check if 'cdr.exe' is running
+if check_process('CDR.EXE'):
+    print('CDR.EXE is running.')
+else:
+    subprocess.Popen(['"' + bosch_filepath + '"'])
+    
+
