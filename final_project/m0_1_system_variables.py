70d26a7f10f87f9d4ca3c1c73d42942f106b4277
WCraig <38740322+crgsquared@users.noreply.github.com>
2023-04-13 17:14:45 -0700

    Using import to run the modules from control.

--- final_project/m0_1_system_variables.py
+++ final_project/m0_1_system_variables.py
@@ -3,20 +3,5 @@ import os
 
 raw_data_path = r'H:\LOSS1\EDR\Downloaded Files'
 
-bins = 10
-
 bosch_filepath = os.path.join('C:', 'Program Files (x86)', 'Bosch', 'Crash Data Retrieval', 'CDR.EXE')
 
-#This is the list of files that have already been imported into the system.
-scanned_file_list = []
-
-#This is a 'to-do' for files that need to be converted.
-file_staging_list = []
-
-complete_file_list = []
-
-complete_path_list = []
-
-cdrx_path_list = []
-
-unaccompanied_cdrx_path_list = []
