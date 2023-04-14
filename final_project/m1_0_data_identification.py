5de2e544075fb2d957a952c9e0eb4741523f24e7
WCraig <38740322+crgsquared@users.noreply.github.com>
2023-04-13 17:04:52 -0700

    Further update

--- final_project/m1_0_data_identification.py
+++ final_project/m1_0_data_identification.py
@@ -1,13 +1,23 @@
 import os
-from m0_1_system_variables import raw_data_path, scanned_file_list, file_staging_list, cdrx_path_list
+from m0_1_system_variables import raw_data_path, cdrx_path_list, unaccompanied_cdrx_path_list
 
 
-file_count = 0
+#This block of code creates a list of paths to cdrx files which do not have an accompanying .CSV file. 
+#The objective of this module is to find all of these 'unaccompanied' files and then create a .CSV version suitable for import and analysis. 
+
+#Whenever the database is re-scanned for new files, the unaccompanied_cdrx_path_list must be cleared. Otherwise, it will simply add the new list of unaccompanied files to an old list.
+unaccompanied_cdrx_path_list = []
+
+
 for root, dirs, files in os.walk(raw_data_path):
         for filename in files:
-                if filename.lower().endswith('cdrx'):     
+                if filename.lower().endswith('cdrx'):
+                        csv_filename = os.path.splitext(filename)[0] + '.CSV'
+                        if csv_filename not in files:     
                         # os.path.join is performed to handle the case of one vehicle having multiple records. 
                         # The record is saved as the vehicle's VIN, but successive records bearing the same name are placed in different directories.  
                         # Considering the filepath as part of the record's 'identity' will ensure that these edge cases are accommodated.
-                        cdrx_path_list.append(os.path.join(root, filename))
 
+                            unaccompanied_cdrx_path_list.append(os.path.join(root, filename))
+
+                            
