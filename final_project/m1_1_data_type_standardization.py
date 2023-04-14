356a46edbe31926fd1b032a97bceabc5c6898e37
WCraig <38740322+crgsquared@users.noreply.github.com>
2023-04-14 14:18:17 -0700

    Re-commit

--- final_project/m1_1_data_type_standardization.py
+++ final_project/m1_1_data_type_standardization.py
@@ -1,14 +1,15 @@
-import pyautogui
 import psutil
 import sys
 import subprocess
+import pandas as pd
 from m0_1_system_variables import bosch_filepath
 
+'''This block is used to determine whether the Bosch CDR tool is running'''
 
 def check_process(process_name):
     """Check if a process is running on Windows"""
     for proc in psutil.process_iter(['name']):
-        if proc.info['name'] == process_name:
+        if proc.name() == process_name:
             return True
     return False
     sys.exit(1)
@@ -18,6 +19,10 @@ if check_process('CDR.EXE'):
     print('CDR.EXE is running.')
 else:
     process = subprocess.Popen(bosch_filepath)
+
+'''This block is used to create a pandas dataframe from the csv file containing all not-yet-interpreted .CDRx files'''
+with open ('unaccompanied_cdrx_path_list.csv', 'r') as path_list:
+    complete_records_dataframe = pd.read_csv(path_list)
 
 
 
