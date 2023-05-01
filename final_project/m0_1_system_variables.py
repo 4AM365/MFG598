6deeadf75abd048d1590b942abdb476e9729365d
4AM365 <wtcraig@live.com>
2023-04-30 18:15:57 -0700

    DONE

--- final_project/m0_1_system_variables.py
+++ final_project/m0_1_system_variables.py
@@ -2,6 +2,6 @@
 import os
 
 # raw_data_path = r'H:\LOSS1\EDR\Downloaded Files'
-test_data_path = r'C:\Users\Craig UHES\Desktop\downloaded_files_experimental_copy'
+test_data_path = r'C:\Code\MFG598\final_project\test_cdr_directory'
 
 bosch_filepath = r'C:\Program Files (x86)\Bosch\Crash Data Retrieval\CDR.EXE'
