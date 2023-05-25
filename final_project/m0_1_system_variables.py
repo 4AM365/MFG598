bbfdc90319216eb320a1f43c9c733f419c4d1e16
WCraig <38740322+crgsquared@users.noreply.github.com>
2023-05-25 13:52:16 -0700

    altered gitignore & added to test file repo

--- final_project/m0_1_system_variables.py
+++ final_project/m0_1_system_variables.py
@@ -2,6 +2,6 @@
 import os
 
 # raw_data_path = r'H:\LOSS1\EDR\Downloaded Files'
-test_data_path = r'C:\Code\MFG598\final_project\test_cdr_directory'
+test_data_path = r''
 
 bosch_filepath = r'C:\Program Files (x86)\Bosch\Crash Data Retrieval\CDR.EXE'
